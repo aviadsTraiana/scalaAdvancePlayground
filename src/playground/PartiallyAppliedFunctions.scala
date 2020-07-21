4b2ded1b6cd772c724bd10c2ca9b85c6bd7e9262
aviadsTraiana <aviads@traiana.com>
2020-07-21 12:43:37 +0300

    playing with PAF

---
+++ src/playground/PartiallyAppliedFunctions.scala
@@ -0,0 +1,11 @@
+package playground
+
+object PartiallyAppliedFunctions extends App {
+
+  val numbers= List(Math.PI,Math.E,1,9.8,1.3e-12)
+  def curriedFormatter(s:String)(d:Double) :String = s.format(d)
+  val simpleFormatter = curriedFormatter("%4.2f") _
+  val seriousFormatter = curriedFormatter("%8.6f") _
+  val preciseFormatter = curriedFormatter("%14.12f") _
+  println(numbers.map(preciseFormatter))
+}
