f675fc0376b3c4286205d5b3dccd0640a381ebe5
aviadsTraiana <aviads@traiana.com>
2020-07-22 12:39:56 +0300

    improving lazy monad

--- src/exercises/LazyMonad.scala
+++ src/exercises/LazyMonad.scala
@@ -1,9 +1,11 @@
 package exercises
 
 class LazyMonad[+A](x: ⇒ A) {
-
-  def evaluate = x
-  def flatMap[B](f: (⇒A) ⇒ LazyMonad[B]):LazyMonad[B] = f(x)
+  private lazy val xByNeed= x
+  def evaluate = xByNeed
+  def flatMap[B](f: (⇒A) ⇒ LazyMonad[B]):LazyMonad[B] = f(xByNeed)
+  def map[B](f: A⇒B):LazyMonad[B] = flatMap(x⇒ LazyMonad(f(x)))
+  def flatten[B](m:LazyMonad[LazyMonad[B]]) :LazyMonad[B] = m.flatMap((x:LazyMonad[B]) ⇒ x)
 }
 
 object LazyMonad{
@@ -17,9 +19,24 @@ object TestLaziness extends App{
   }
   println("no evaluation until now")
   println(lazyMonad.evaluate)
+  println("------------------")
   val lazy2=lazyMonad.flatMap(x⇒ LazyMonad{x*2})
+  val lazy3=lazyMonad.flatMap(x⇒ LazyMonad{x*2})
+
   println("no evaluation until now")
-  lazy2.evaluate
+  println(lazy2.evaluate)
+  println(lazy3.evaluate)
+
+  //monads laws
+
+
+//  //left
+//  println(LazyMonad(x).flatMap(f) == f(x))
+//  //right
+//  println(LazyMonad(x).flatMap(x⇒ LazyMonad(x))==LazyMonad(x))
+//  //associativity
+//  println(LazyMonad(x).flatMap(f).flatMap(g) == LazyMonad(x).flatMap(x⇒ f(x).flatMap(g)))
+
 
 
 }
